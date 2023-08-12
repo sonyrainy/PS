fae3e330dd837965573c59e5628e31531da2886f
손현진 son_hyeonjin <91364766+sonyrainy@users.noreply.github.com>
2023-08-12 19:52:31 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,28 @@
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<long> d(1001);
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    d[1] = 1;
+    d[2] = 2;
+
+    for (int i = 3; i <= n; i++) {
+        d[i] = (d[i - 1] + d[i - 2])%10007;
+    }
+    cout << d[n];
+
+
+    return 0;
+}
