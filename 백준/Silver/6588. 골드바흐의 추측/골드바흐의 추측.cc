663b546129a4741c25ccabb118b46b0c62b189d4
Son_HyeonJin <91364766+sonyrainy@users.noreply.github.com>
2024-08-13 11:38:27 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 60 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.cc
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.cc
@@ -1,50 +1,46 @@
-#include <iostream>
-#include <vector>
 #include <cmath>
+#include <iostream>
 
 using namespace std;
 
-const int MAX = 1000000;
-vector<bool> is_prime(MAX + 1, true);
-
-void sieve() {
-    is_prime[0] = is_prime[1] = false; // 0과 1은 소수가 아님
-    for (int i = 2; i * i <= MAX; ++i) {
-        if (is_prime[i]) {
-            for (int j = i * i; j <= MAX; j += i) {
-                is_prime[j] = false;
-            }
-        }
-    }
-}
+int is_sosu[1000001]; // 0이면 소수, 1이면 소수 아님
 
 int main() {
-    ios_base ::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
 
-    sieve(); // 소수를 미리 계산
+  // 초기화: 0과 1은 소수가 아님
+  is_sosu[0] = is_sosu[1] = 1;
 
-    int n;
-    while (true) {
-        cin >> n;
-        if (n == 0) {
-            break;
-        }
-        
-        bool found = false;
-        for (int i = 3; i <= n / 2; i += 2) {
-            if (is_prime[i] && is_prime[n - i]) {
-                cout << n << " = " << i << " + " << n - i << "\n";
-                found = true;
-                break;
-            }
-        }
-
-        if (!found) {
-            cout << "Goldbach's conjecture is wrong.\n";
-        }
+  // 에라토스테네스의 체
+  for (int i = 2; i <= sqrt(1000000); i++) {
+    if (is_sosu[i] == 0) { // i가 소수라면
+      for (int j = i * i; j <= 1000000; j += i) {
+        is_sosu[j] = 1; // i의 배수들은 소수가 아님
+      }
     }
+  }
 
-    return 0;
+  int n;
+  while (true) {
+    cin >> n;
+    if (n == 0) {
+      break;
+    }
+    bool found = false;
+
+    for (int i = 3; i <= n / 2; i += 2) {
+      if (is_sosu[n - i] == 0 && is_sosu[i] == 0) { // 두 수가 모두 소수라면
+        cout << n << " = " << i << " + " << n - i << "\n";
+        found = true;
+        break;
+      }
+    }
+    if (!found) {
+      cout << "Goldbach's conjecture is wrong."
+           << "\n";
+    }
+  }
+  return 0;
 }
