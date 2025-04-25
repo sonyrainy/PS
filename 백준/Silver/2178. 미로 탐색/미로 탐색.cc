98af71a388e52d21bf14a418097caf27371d7b04
Son Hyeon-Jin <91364766+sonyrainy@users.noreply.github.com>
2025-04-26 07:53:17 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2072 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -1,69 +1,63 @@
 #include <iostream>
 #include <queue>
-#include <vector>
 #include <stdio.h>
+#include <vector>
 
 using namespace std;
 
-int Min = 2147000000;
 int n, m;
-//int map[n][m], visited[n][m];
-//map의 값을 받을 때, 숫자가 붙은 상태로 받기 때문에 char로 받아 하나하나 뜯는다.
 
-//vector<vector<char>> map(n,vector<char>(n, '0'));
-//vector<vector<int>> visited(n, vector<int>(m, 0));
+//방문 여부
+int visited[101][101];
+//실제 값 받을 곳
 char map[101][101];
 
-//방문 여부용
-int visited[101][101];
-
-//방문 횟수용
-int check[101][101];
-
-int dx[4] = { -1, 0, 1, 0 };
-int dy[4] = { 0, 1, 0 , -1 };
+int dx[4] = {-1, 0, 1, 0};
+int dy[4] = {0, 1, 0, -1};
 
 void BFS(int x, int y) {
-    int xx, yy;
-    visited[x][y] = 1;
+  int xx, yy;
+  visited[x][y] = 1;
 
-    queue<pair<int, int>> q;
-    q.push(make_pair(x, y));
+  queue<pair<int, int>> q;
+  q.push(make_pair(x, y));
 
-    // 큐가 비어있을 때까지 반복
-    while (q.empty() == false) {
-        x = q.front().first;
-        y = q.front().second;
+  // 큐가 비어있을 때까지 반복
+  while (q.empty() == false) {
+    x = q.front().first;
+    y = q.front().second;
 
-        q.pop();
-        for (int i = 0; i < 4; i++) {
-            xx = x + dx[i];
-            yy = y + dy[i];
+    q.pop();
 
-            // 미로의 내부 안에서
-            if (xx >= 0 && xx < n && yy >= 0 && yy < m) {
+    for (int i = 0; i < 4; i++) {
+      xx = x + dx[i];
+      yy = y + dy[i];
 
-                // 방문된 적 없고, 지나갈 수 있는 곳이라면
-                if (visited[xx][yy] == 0 && map[xx][yy] == '1') {
-                    check[xx][yy] = check[x][y] + 1;
-                    visited[xx][yy] = 1;
-                    q.push(make_pair(xx, yy));
-                }
-            }
+      if (xx >= 0 && xx < n && yy >= 0 && yy < m) {
+
+        // 방문된 적 없고, 지나갈 수 있는 곳이라면
+        if (visited[xx][yy] == 0 && map[xx][yy] == '1') {
+          visited[xx][yy] = visited[x][y] + 1;
+          q.push(make_pair(xx, yy));
         }
+      }
     }
+  }
 }
 
 int main() {
-    cin >> n >> m;
+  cin >> n >> m;
 
-    for (int i = 0; i < n; i++) {
-        cin >> map[i];
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      cin >> map[i][j];
     }
+  }
+  visited[0][0] = 1;
 
-    BFS(0, 0);
+  BFS(0, 0);
 
-    cout << check[n-1][m-1]+1;
+  cout << visited[n - 1][m - 1];
 
-    return 0;
+  return 0;
 }
