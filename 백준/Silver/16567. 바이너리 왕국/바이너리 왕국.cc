c62f10185b55fd30b7a4572ac78efa81a3580217
Son Hyeon-Jin <91364766+sonyrainy@users.noreply.github.com>
2025-05-30 22:02:39 +0900

    Update 바이너리 왕국.cc

--- 백준/Silver/16567. 바이너리 왕국/바이너리 왕국.cc
+++ 백준/Silver/16567. 바이너리 왕국/바이너리 왕국.cc
@@ -1,6 +1,4 @@
-#include <iostream>
-#include <vector>
-#include <string>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
@@ -20,7 +18,8 @@ int main() {
         if (status[i] == 1 && !conti) {
             cnt++;
             conti = true;
-        } else if (status[i] == 0) {
+        }
+        else if (status[i] == 0) {
             conti = false;
         }
     }
@@ -31,29 +30,31 @@ int main() {
         cin >> trial;
         if (trial == 0) {
             cout << cnt << "\n";  // 연속된 1의 개수 출력
-        } else {
+        }
+        else {
             int index;
             cin >> index;
-            index--;  // 1-based index를 0-based index로 변경
+            index--;
 
             if (status[index] == 1) continue;  // 이미 1이면 아무 일도 하지 않음
 
             status[index] = 1;  // 해당 인덱스를 1로 설정
 
-            // 첫 번째 요소일 경우
+            // 첫 번째 인덱스일 경우
             if (index == 0) {
-                if (status[index + 1] == 0) cnt++;  // 1이 하나만 연속되도록
+                if (status[index + 1] == 0) cnt++;
             }
-            // 마지막 요소일 경우
+            // 마지막 인덱스일 경우
             else if (index == N - 1) {
-                if (status[index - 1] == 0) cnt++;  // 1이 하나만 연속되도록
+                if (status[index - 1] == 0) cnt++;
             }
-            // 중간 요소일 경우
+            // 중간 인덱스일 경우
             else {
                 if (status[index - 1] == 0 && status[index + 1] == 0) {
                     cnt++;  // 양 옆이 0이면 새로운 연속된 1 구간 추가
-                } else if (status[index - 1] == 1 && status[index + 1] == 1) {
-                    cnt--;  // 양 옆이 1이면 연속된 1 구간이 사라짐
+                }
+                else if (status[index - 1] == 1 && status[index + 1] == 1) {
+                    cnt--;  // 양 옆이 1이면 연속된 1 구간 감소
                 }
             }
         }
