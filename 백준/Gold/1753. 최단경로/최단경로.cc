75e28246046de631b9d2abaf8e53e9cb739f4ec9
Son Hyeon-Jin <91364766+sonyrainy@users.noreply.github.com>
2025-07-30 12:23:28 +0900

    [Gold IV] Title: 최단경로, Time: 88 ms, Memory: 9184 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.cc
+++ 백준/Gold/1753. 최단경로/최단경로.cc
@@ -1,96 +1,59 @@
-#include <algorithm>
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <stdio.h>
+#include <bits/stdc++.h>
 
 using namespace std;
-int INF = 2147000000;
 
-// 간선 정보를 저장하는 벡터
-vector<pair<int, int>> a[300001];
+vector<pair<int, int>> adj[20005];
+int d[20005];
 
-// 최단 거리를 저장하는 벡터
-vector<int> d(20001);
-
-// 다익스트라 실행 함수
-void dijkstra(int startIndex) {
-
-    // 시작점에서 시작점으로의 최소 거리는 0
-    d[startIndex] = 0;
-
-    // min heap 구조, 더 작은 값을 기준으로 최상단 노드를 만들도록 한다.
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-
-    // 출발 노드의 정보를 우선순위 큐에 넣는다.
-    pq.push(make_pair(0, startIndex)); 
-    // 우선순위 큐는 first 값을 기준 정렬되므로, 위와 같이 수정한다.
-    
-    while (pq.empty()==false) {
-
-        // 기준 점부터 가장 최소 거리 노드의 index
-        int current = pq.top().second;
-
-        // 기준 점부터 가장 최소 거리 노드의 값
-        int distance = pq.top().first;
-        pq.pop();
-
-        // 최단 거리가 아닌 경우 스킵
-        if (d[current] < distance) continue;
-
-		// pq 내부에 여러 개가 들어가도록 하고, 위 코드에서 가장 최소에 대한 값을
-		// distance, current에 넣는다.
-        for (int i = 0; i < a[current].size(); i++) { 
-
-            // 선택된 노드의 인적노드
-            int nextIndex = a[current][i].first;
-
-            // 선택된 노드를 거쳐서 인접 노드로 가는 거리
-            int nextDistance = distance + a[current][i].second;
-
-            // 기존 최소 비용보다 더 작다면 교체
-            if (nextDistance < d[nextIndex]) {
-                d[nextIndex] = nextDistance;
-
-                // 그렇게 새롭게 갱신된 값을 넣어준다.
-                pq.push(make_pair(nextDistance, nextIndex));
-            }
-        }
-    }
-}
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
 
-    int v, e;
-    cin >> v >> e;
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int V, E, st;
+	cin >> V >> E >> st;
 
-    int startN;
-    cin >> startN;
+	for (int i = 1; i <= E; ++i){
+		int u, v, w;
+		cin >> u >> v >> w;
+		adj[u].push_back({ v, w });
+	}
 
- 
-    for (int i = 1; i <= v; i++) {
-        d[i] = INF;
-    }
+	for (int i =1;i<=V;++i){
+		d[i] = 2147000000;
+	}
 
-    for (int i = 0; i < e; i++) {
-        int start, end, cost;
-        cin >> start >> end >> cost;
-        a[start].push_back(make_pair(end, cost));
-    }
+	priority_queue < pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
 
+	pq.push({ 0, st });
+	d[st] = 0;
 
-    dijkstra(startN);
+	while (!pq.empty()) {
 
-    for (int i = 1; i <= v; i++) {
-        if (d[i] == INF) {
-            cout << "INF" << '\n';
-        }
-        else {
-            cout << d[i] << '\n';
-        }
-    }
+		// 시작점에서 도착하는 지점 가중치
+		int w = pq.top().first;
 
-    return 0;
+		// 도착하는 지점
+		int v = pq.top().second;
+
+		pq.pop();
+
+		// 미만이어야 함. 그래야지 다른 곳을 통해 갈 수 있는
+		// 길도 고려할 수 있다.
+		if (d[v] < w) continue;
+
+		// u(idx), v, w
+		for (auto nxt:adj[v])
+		{
+			if (d[nxt.first] <= d[v] + nxt.second) continue;
+			d[nxt.first] = d[v] +nxt.second;
+			pq.push({ d[nxt.first] , nxt.first});
+		}
+	}
+
+	for (int i = 1; i <= V; ++i) {
+		if (d[i] == 2147000000) { cout << "INF" << "\n"; continue; }
+		cout << d[i] << "\n";
+
+	}
+	return 0;
 }
